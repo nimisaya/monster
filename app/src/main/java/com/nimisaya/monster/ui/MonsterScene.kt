c3864ae9da5b23f1bf9782c05e2db6996b842ca0
Amanda Jarvinen <nimisaya@users.noreply.github.com>
2023-01-29 16:06:38 +1100

    Create background

--- app/src/main/java/com/nimisaya/monster/ui/MonsterScene.kt
+++ app/src/main/java/com/nimisaya/monster/ui/MonsterScene.kt
@@ -1,7 +1,12 @@
 package com.nimisaya.monster.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,15 +16,21 @@ import com.nimisaya.monster.ui.monster.Monster
 @Composable
 fun MonsterScene(modifier: Modifier = Modifier) {
     Box(modifier = modifier){
-        Monster(
-            size = with(LocalDensity.current) { 150.dp.toPx() }.toInt(),
-            strokeThickness = with(LocalDensity.current) { 3.dp.toPx() }
-        )
         Background()
+        Column(
+            modifier = Modifier.padding(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Spacer(modifier = Modifier.height(48.dp))
+            Monster(
+                size = with(LocalDensity.current) { 150.dp.toPx() }.toInt(),
+                strokeThickness = with(LocalDensity.current) { 3.dp.toPx() }
+            )
+        }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview
 @Composable
 private fun MonsterScenePreview() {
     MonsterScene()
